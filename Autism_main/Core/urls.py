c4863f4ae1cf5ee3d509d4c60c132da85e560c44
Prashanth HV <prashanthhv2003@gmail.com>
2025-07-30 00:00:34 +0530

    sdsds

--- Autism_main/Core/urls.py
+++ Autism_main/Core/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlspatterns= [
+    path('', views.Home, name='home'),
+    path('register/', views.RegisterView, name='register'),
+    path('login/', views.LoginView, name='login'),
     
 ]
--- Autism_main/Core/views.py
+++ Autism_main/Core/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.conf import settings
+from django.core.mail import EmailMessage
+from django.utils import timezone
+from django.urls import reverse
+from .models import *
 
-# Create your views here.
+def Home(request):
+    return render(request, 'index.html')
+
+def RegisterView(request):
+    return render(request, 'register.html')
+
+def LoginView(request):
+    return render(request, 'login.html')
